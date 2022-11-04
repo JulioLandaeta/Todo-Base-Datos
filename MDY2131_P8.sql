e734d517d7f0ea911502459ef9b0380d8164b9dc
JulioLandaeta <jul.landaeta@duocuc.cl>
2022-11-03 21:34:37 -0300

    Guia 8

---
+++ MDY2131_P8.sql
@@ -0,0 +1,19 @@
+SELECT
+    ts.descripcion||'.'||s.descripcion AS SISTEMA_SALUD,
+    COUNT(*) AS TOTAL_ATENCIONES
+FROM ATENCION a
+    JOIN PACIENTE p ON a.pac_run = p.pac_run
+    JOIN SALUD s ON p.sal_id = s.sal_id
+    JOIN TIPO_SALUD ts ON s.tipo_sal_id = ts.tipo_sal_id
+WHERE EXTRACT(YEAR FROM a.fecha_atencion) = EXTRACT(YEAR FROM SYSDATE)
+    AND EXTRACT(MONTH FROM a.fecha_atencion) = 9
+GROUP BY ts.descripcion, s.descripcion
+HAVING COUNT(*) > (SELECT AVG(COUNT(*))
+    FROM ATENCION a
+    JOIN PACIENTE p ON a.pac_run = p.pac_run
+    JOIN SALUD s ON p.sal_id = s.sal_id
+    JOIN TIPO_SALUD ts ON s.tipo_sal_id = ts.tipo_sal_id
+WHERE EXTRACT(YEAR FROM a.fecha_atencion) = EXTRACT(YEAR FROM SYSDATE)
+    AND EXTRACT(MONTH FROM a.fecha_atencion) = 9
+GROUP BY ts.descripcion, s.descripcion)
+;
